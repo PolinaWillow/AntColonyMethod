c63d5f0d977af543d32cd1869470f8c4045e0505
polianna_654@mail.ru <polianna_654@mail.ru>
2023-02-12 21:28:48 +0300

    Разметка основных страниц

---
+++ AntColonyClient/Pages/ClientPages/AddNewTask.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AntColonyClient.Pages.ClientPages
+{
+    public class AddNewTaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- AntColonyClient/Pages/Index.cshtml.cs
+++ AntColonyClient/Pages/Index.cshtml.cs
@@ -14,7 +14,6 @@ namespace AntColonyClient.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
